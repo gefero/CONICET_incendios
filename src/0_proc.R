12032e0af563140dc08c76b806b0164cd459cdcb
German Rosati <german.rosati@gmail.com>
2022-03-15 12:09:52 -0300

    Agrego analisis preliminar

--- src/0_proc.R
+++ src/0_proc.R
@@ -1,35 +1,60 @@
 library(tidyverse)
 library(sf)
 
-
 f <- function(x){1 / (1 + exp(-x))}
 soft_sign_func <- function(x){ x / (1 + abs(x))  }
 
 incendios <- read_csv('./data/raw/arg_focos_grilla.csv') %>%
+                select(-c(A_2022:A_2025)) %>%
                 rename(wkt = the_geom) %>%
                 mutate(across(starts_with("A_"), ~replace_na(.x, 0))) %>%
                 mutate(focos_media = focos / anos )
-                
-incendios <- incendios %>%
+
+frontera_depto <- read_sf('../CONICET_estr_agr/proc_data/tablas_finales/frontera_depto.geojson')
+
+incendios %>%
+        select(-anos) %>%
+        pivot_longer(starts_with('A'), names_to = 'ano') %>%
+        mutate(ano = as.numeric(str_remove(ano, 'A_'))) %>%
+        group_by(ano) %>%
+        summarise(n = sum(value)) %>%
+        ggplot() +
+                geom_line(aes(x=ano, y=n), group=1) +
+                theme_minimal()
+
+incendios_geom <- incendios %>%
         select(-starts_with("A_")) %>%
         filter(focos > 0 ) %>%
         st_as_sf(wkt="wkt")
 
-incendios %>%
+incendios_geom %>%
         ggplot() +
-        geom_sf(aes(fill=soft_sign_func(focos)), colour=NA) +
+        geom_sf(aes(fill=(focos)), color=NA) +
         scale_fill_viridis_c() +
         theme_minimal()
         
-incendios %>%
+incendios_geom %>%
         ggplot() +
         geom_sf(aes(fill=(focos_media)), colour=NA) +
         scale_fill_viridis_c() +
         theme_minimal()
 
+incendios_geom %>%
+        st_set_geometry(NULL) %>%
+        summarise(mean = mean(focos),
+                  sd = sd(focos),
+                  cv = sd/mean,
+                  q1 = quantile(focos, probs=0.25),
+                  q2 = quantile(focos, probs=0.5),
+                  q3 = quantile(focos, probs=0.75),
+                  p70 = quantile(focos, probs=0.7),
+                  p80 = quantile(focos, probs=0.80),
+                  p90 = quantile(focos, probs=0.9),
+                  p99 = quantile(focos, probs=0.99),
+                  mad = mad(focos))
 
-frontera_depto <- read_sf('../CONICET_estr_agr/proc_data/tablas_finales/frontera_depto.geojson')
-st_crs(incendios_centr)
+
+
 
 incendios_centr <- incendios %>%
         st_centroid() %>%
